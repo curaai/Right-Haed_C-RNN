890306ebefe3ff49c59fe1d495e191fdf4c45d46
curaai <curaai00@gmail.com>
2018-01-12 15:33:38 +0900

    iou

--- RPN.py
+++ RPN.py
@@ -1,6 +1,23 @@
 import numpy as np
 
 
+# get iou between two boxes
+# it's x1, y1, x2, y2. Not x, y, w h
+def intersection_over_union(box1, box2):
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+
+    area_intersection = (x2 - x1) * (y2 - y1)
+    area_box1 = (box1[2] - box1[0]) * (box1[3] - box1[2])
+    area_box2 = (box2[2] - box2[0]) * (box2[3] - box2[2])
+    area_union = area_box1 + area_box2 - area_intersection
+
+    iou = area_intersection / area_union
+    return iou
+
+
 def generate_anchor(feat_shape, image_shape, anchor_sizes, anchor_ratios):
     """
     s: stride
